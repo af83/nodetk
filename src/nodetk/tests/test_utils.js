cd7da56e061bda474fbe7e7f3fcf7b71ce30742d
Virtuo <pierre@ruyssen.fr>
2011-01-17 10:30:55 +0100

    Add deep_extend to utils functions.

--- src/nodetk/tests/test_utils.js
+++ src/nodetk/tests/test_utils.js
@@ -30,12 +30,53 @@ exports.tests = [
   var target = {name: 'toto'};
   var obj1 = {lastname: 'titi'};
   var obj2 = {tutu: 'tutu'};
-  var res = utils.extend(target, {lastname: 'titi'}, {tutu: 'tutu'});
+  var res = utils.extend(target, obj1, obj2);
   assert.deepEqual(res, {name: 'toto', lastname:'titi', tutu: 'tutu'});
   assert.equal(res, target);
   assert.deepEqual(obj1, {lastname: 'titi'});
   assert.deepEqual(obj2, {tutu: 'tutu'});
 }],
+
+
+['deep extend with nothing', 1,function() {
+  var res = utils.deep_extend();
+  assert.deepEqual(res, {});
+}],
+
+['deep extend only target', 2, function() {
+  var target = {name: 'toto'};
+  var res = utils.extend(target);
+  assert.deepEqual(res, target);
+  assert.equal(res, target);
+}],
+
+['deep extend target with one obj', 3, function() {
+  var target = {name: 'toto', friends: ['riri'], mother: {name: "Mimi"}};
+  var obj1 = {lastname: 'titi', friends: ['fifi', 'loulou'], mother: {id: 1}};
+  var res = utils.deep_extend(target, obj1);
+  assert.deepEqual(res, {
+    name: 'toto'
+  , lastname:'titi'
+  , mother: {id: 1, name: "Mimi"}
+  , friends: ['fifi', 'loulou']
+  });
+  assert.equal(res, target);
+  assert.deepEqual(obj1, 
+         {lastname: 'titi', friends: ['fifi', 'loulou'], mother: {id: 1}});
+}],
+
+['deep extend target with two objs', 2, function() {
+  var target = {e: 'e'};
+  var obj1 = {lastname: 'titi', a: {b: {c: 'c'}}};
+  var obj2 = {tutu: 'tutu', a: {b: {d: 'd'}}};
+  var res = utils.deep_extend(target, obj1, obj2);
+  assert.deepEqual(res, {
+    e: 'e', lastname: 'titi', tutu: 'tutu',
+    a: {b: {c: 'c', d: 'd'}}
+  });
+  assert.equal(res, target);
+}],
+
 
 ['count properties', 3, function() {
   assert.equal(utils.count_properties({}), 0);
--- src/nodetk/utils.js
+++ src/nodetk/utils.js
@@ -12,8 +12,29 @@ exports.extend = function(target, obj1, obj2) {
   target = target || {};
   var objs = Array.prototype.slice.apply(arguments, [1]);
   objs.forEach(function(obj) {
-    for(attr in obj) {
+    for(var attr in obj) {
       target[attr] = obj[attr];
+    }
+  });
+  return target;
+};
+
+var deep_extend = exports.deep_extend = function(target, obj1, obj2) {
+  /* Deeply merge obj1 and obj2 (and any other given obj) in target 
+   * Array are not considered as objects, so the last arriving will 
+   * override any already defined array. 
+   */
+  target = target || {};
+  var objs = Array.prototype.slice.apply(arguments, [1]);
+  objs.forEach(function(obj) {
+    for(var attr in obj) {
+      var val = obj[attr];
+      if(typeof val == "object" && !isArray(val)) {
+        target[attr] = deep_extend(target[attr] || {}, val);
+      }
+      else {
+        target[attr] = val;
+      }
     }
   });
   return target;
@@ -27,7 +48,7 @@ exports.count_properties = function(obj) {
   return count;
 }
 
-exports.isArray = function(obj) {
+var isArray = exports.isArray = function(obj) {
   /** Returns True if obj is an Array.
    */
   return obj.constructor == Array;
